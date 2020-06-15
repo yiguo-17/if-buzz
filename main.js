72f06e6fb27b55ac0ec7171ed72164fde0d139ef
play.js <git@gituser.com>
2020-06-15 19:48:41 -0400

    add FIZZBUZZ  application

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 /*******************************************************************************
  * Your code here!
  ******************************************************************************/
-
-
+ if(typeof(parseInt(getInput(1)))==='number'){let dv1 = parseInt(getInput(1));}else {let dv1=3;}
+  if(typeof(parseInt(getInput(2)))==='number'){let dv2 = parseInt(getInput(2));}else {let dv2=5;}
+ const rm1 = parseInt(getInput(0)) % dv1;
+ const rm2 = parseInt(getInput(0)) % dv2;
+ if(rm1===0 && rm2===0){console.log('FizzBuzz');} 
+else if(rm1===0){console.log('Fizz');}
+else if(rm2===0){console.log('Buzz');}
+else {console.log(getInput(0));}
 
 
 
